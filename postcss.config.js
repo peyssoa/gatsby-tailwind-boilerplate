7e496010189ee46333a82aa9f90781bc79858a34
peyssoa <peyssoa@gmail.com>
2020-10-23 21:06:41 +0800

    feat: add postcss and tailwind configs

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+   plugins: {
+      autoprefixer: {},
+      'postcss-preset-env': {
+         stage: 0,
+      },
+      'postcss-nesting': {},
+      tailwindcss: {},
+   },
+}
